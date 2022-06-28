1338960409d8c54f7f1d83d1e5d90f2e7fa1f5d1
= <=>
2022-06-28 18:32:45 +0300

    create annotation

---
+++ jsonschema-module-jackson/src/main/java/com/github/victools/jsonschema/module/jackson/extended/annotation/JsonPropertyTitle.java
@@ -0,0 +1,15 @@
+package src.main.java.com.github.victools.jsonschema.module.jackson.extended.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation allows to define a title for the property.
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JsonPropertyTitle {
+    String value() default "title default";
+}
